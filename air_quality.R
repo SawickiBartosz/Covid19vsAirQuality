41084be21af76f57475f6452c4e4367858d6f4ce
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2020-11-23 23:02:00 +0100

    apple mobility

--- air_quality.R
+++ air_quality.R
@@ -1,3 +1,6 @@
+# Te dane koncza sie na styczniu 2020 chyba nieuzywalne
+
+
 library(XML)
 library(methods)
 library(ggplot2)
@@ -12,11 +15,13 @@ source_links <- c("https://ereporting.blob.core.windows.net/downloadservice/PL_8
 "https://ereporting.blob.core.windows.net/downloadservice/PL/PL_8_29748_2020_timeseries.csv" ,
 "https://ereporting.blob.core.windows.net/downloadservice/PL/PL_8_64601_2020_timeseries.csv")
 
-data <- read.csv("https://ereporting.blob.core.windows.net/downloadservice/PL_8_29500_2019_timeseries.csv")
+raw_data <- read.csv("https://ereporting.blob.core.windows.net/downloadservice/PL_8_29500_2019_timeseries.csv")
 
 for(link in source_links){
   df <- read.csv(link)
-  bind_rows(data,df)
+  bind_rows(raw_data,df)
 }
 
-ggplot(data = data, aes(x = DatetimeBegin)) + geom_point(aes(y = Concentration))
+data <- raw_data %>% select(DatetimeBegin,Concentration) %>% mutate(date = as.Date(DatetimeBegin)) %>% select(date,Concentration) %>% group_by(date) %>% summarise(mean = mean(Concentration))
+
+ggplot(data = data, aes(x = date)) + geom_line(aes(y = mean, group = mean))
